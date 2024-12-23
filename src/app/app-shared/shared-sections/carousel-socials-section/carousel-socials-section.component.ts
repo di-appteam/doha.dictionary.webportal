841bc2a3fa47b270966cc4928e0d1bc0cd67a41f
ahmed elhadad <ahmed.elhadad@dohainstitute.edu.qa>
2024-12-23 11:57:59 +0300

    fix issues

--- src/app/app-shared/shared-sections/carousel-socials-section/carousel-socials-section.component.ts
+++ src/app/app-shared/shared-sections/carousel-socials-section/carousel-socials-section.component.ts
@@ -17,45 +17,45 @@ export class CarouselSocialsSectionComponent {
   socialData:any[]=[
     {
       "postUrl": "https://twitter.com/dohainstitute/status/https://x.com/dohainstitute/status/1853710771064385561/",
-      "imageUrl": "https://www.dohainstitute.edu.qa/admin/Lists/SocialMedia/Attachments/152380/11815116.jpg",
+      "imageUrl": "assets/images/social-static/11769799.jpg",
       "hoverContent": "يقدم مركز اللغات في معهد الدوحة للدراسات العليا ورش تدريبية متنوعة حول اختبار آيلتس (IELTS) من ضمنها: عرض عام لمهارة القراءة في اختبار آيلتس، واستراتيجيات القراءة في الاختبار.\n\nThe Language Center at the DI offers various workshops on the IELTS test. We are offering now IELTS https://t.co/a87BZoc4Al",
       "altText": "11815116",
       "platformIcon": "/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
     },
     {
       "postUrl": "https://twitter.com/dohainstitute/status/https://x.com/dohainstitute/status/1857034672682803314/",
-      "imageUrl": "https://www.dohainstitute.edu.qa/admin/Lists/SocialMedia/Attachments/152373/12247069",
+      "imageUrl": "assets/images/social-static/12448451.jpg",
       "hoverContent": "اليوم التعريفي لبرامج الماجستير والدكتوراه https://t.co/UGZxxNao0X",
       "altText": "12247069",
       "platformIcon": "/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
     },
     {
       "postUrl": "https://twitter.com/dohainstitute/status/https://x.com/dohainstitute/status/1856313514911994224/",
-      "imageUrl": "https://www.dohainstitute.edu.qa/admin/Lists/SocialMedia/Attachments/152376/12045776.jpg",
+      "imageUrl": "assets/images/social-static/12586878.jpg",
       "hoverContent": "زار وفد طلابي من برنامج الماجستير في الدراسات الأمنية النقدية بمعهد الدوحة للدراسات العليا أكاديمية الشرطة في دولة قطر. جاءت الزيارة بهدف التعرف على الجوانب التطبيقية للتحقيق الجنائي والأدلة الجنائية والمؤسسات الإصلاحية والقضائية. https://t.co/JQc8UFFC8A",
       "altText": "12045776",
       "platformIcon": "/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
     },
     {
       "postUrl": "https://twitter.com/dohainstitute/status/https://x.com/dohainstitute/status/1853710771064385561/",
-      "imageUrl": "https://www.dohainstitute.edu.qa/admin/Lists/SocialMedia/Attachments/152380/11815116.jpg",
+      "imageUrl": "assets/images/social-static/12149841.jpg",
       "hoverContent": "يقدم مركز اللغات في معهد الدوحة للدراسات العليا ورش تدريبية متنوعة حول اختبار آيلتس (IELTS) من ضمنها: عرض عام لمهارة القراءة في اختبار آيلتس، واستراتيجيات القراءة في الاختبار.\n\nThe Language Center at the DI offers various workshops on the IELTS test. We are offering now IELTS https://t.co/a87BZoc4Al",
       "altText": "11815116",
       "platformIcon": "/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
     },
     {
       "postUrl": "https://twitter.com/dohainstitute/status/https://x.com/dohainstitute/status/1857034672682803314/",
-      "imageUrl": "https://www.dohainstitute.edu.qa/admin/Lists/SocialMedia/Attachments/152373/12247069",
+      "imageUrl": "assets/images/social-static/12149837.jpg",
       "hoverContent": "اليوم التعريفي لبرامج الماجستير والدكتوراه https://t.co/UGZxxNao0X",
       "altText": "12247069",
       "platformIcon": "/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
     },
     {
       "postUrl": "https://twitter.com/dohainstitute/status/https://x.com/dohainstitute/status/1856313514911994224/",
-      "imageUrl": "https://www.dohainstitute.edu.qa/admin/Lists/SocialMedia/Attachments/152376/12045776.jpg",
+      "imageUrl": "assets/images/social-static/12448456.jpg",
       "hoverContent": "زار وفد طلابي من برنامج الماجستير في الدراسات الأمنية النقدية بمعهد الدوحة للدراسات العليا أكاديمية الشرطة في دولة قطر. جاءت الزيارة بهدف التعرف على الجوانب التطبيقية للتحقيق الجنائي والأدلة الجنائية والمؤسسات الإصلاحية والقضائية. https://t.co/JQc8UFFC8A",
       "altText": "12045776",
-      "platformIcon": "/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
+      "platformIcon": "https://www.dohainstitute.edu.qa/Style Library/DI-Styles/images/SocialMediaSymbol/symbol-Twitter-ar.png"
     }
     // Additional entries can be added here...
   ];
