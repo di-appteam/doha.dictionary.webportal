0ef048810c552645b592032d8d807b714da337fa
Ahmed Samir <a.s.elhaddad@gmail.com>
2025-02-19 12:34:25 +0300

    adding ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.dohadictionary.app',
+  appName: 'معجم الدوحة التاريخي',
+  webDir: 'dist/browser'
+};
+
+export default config;
