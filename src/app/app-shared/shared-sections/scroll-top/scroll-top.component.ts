17e98b5d03868e533d0bc4fa18657140ccf588be
Ahmed Samir <a.s.elhaddad@gmail.com>
2025-02-23 13:24:07 +0300

    fix scroll top issue

--- src/app/app-shared/shared-sections/scroll-top/scroll-top.component.ts
+++ src/app/app-shared/shared-sections/scroll-top/scroll-top.component.ts
@@ -26,16 +26,7 @@ export class ScrollTopComponent  implements OnInit {
   }
 
   scrollToTop() {
-    (function smoothscroll() {
-
-      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-
-      if (currentScroll > 0) {
-        window.requestAnimationFrame(smoothscroll);
-        window.scrollTo(0, currentScroll - (currentScroll / 8));
-      }
-
-    })();
+    window.scrollTo(0, 0);
   }
 
   ngOnInit() {
